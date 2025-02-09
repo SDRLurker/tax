d761a6d1080dcbe71364b295f69d5256b93a0b90
Dong-ryull Shin / Ryan <1982sdr@hanmail.net>
2025-02-10 00:37:01 +0900

    tax 20250126 - modify show.bat

--- show.bat
+++ show.bat
@@ -1,1 +1,6 @@
-pyinstaller --onefile --upx-dir=../upx --icon=icon.ico --exclude-module sqlite3 --exclude-module html --exclude-module unittest --exclude-module matplotlib --exclude-module tkinter --exclude-module pdb --exclude-module ftplib --exclude-module smtplib --exclude-module poplib --hidden-import=openpyxl --hidden-import=openpyxl.cell._writer --add-data "./*;." show.py
+@echo off
+if "%~1"=="" (
+    pyinstaller --onefile --icon=icon.ico --exclude-module sqlite3 --exclude-module html --exclude-module unittest --exclude-module matplotlib --exclude-module tkinter --exclude-module pdb --exclude-module ftplib --exclude-module smtplib --exclude-module poplib --hidden-import=openpyxl --hidden-import=openpyxl.cell._writer --add-data "./*;." show.py
+) else (
+    pyinstaller --onefile --upx-dir="%~1" --icon=icon.ico --exclude-module sqlite3 --exclude-module html --exclude-module unittest --exclude-module matplotlib --exclude-module tkinter --exclude-module pdb --exclude-module ftplib --exclude-module smtplib --exclude-module poplib --hidden-import=openpyxl --hidden-import=openpyxl.cell._writer --add-data "./*;." show.py
+)
